1960c10c052babd62728d52ee7bc7eee1f12883b
benhannel <ben@bensprojects.com>
2017-08-17 14:11:50 +0200

    Fleshed out code for clearing old posts a bit

--- functions/index.js
+++ functions/index.js
@@ -2,15 +2,45 @@ const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
 firebase.initializeApp(functions.config().firebase);
 
-//
+// This is an automatic maintence script that deletes posts which have
+// expired. It is run regularly by a cron job at
+// https://www.setcronjob.com/
+// If a post has a scheduled date, it is deleted 3 days after that date.
+// If a post has no scheduled date, it is deleted 30 days after creation.
 exports.clearOldPosts = functions.https.onRequest((request, response) => {
+	const ONE_DAY = 24 * 60 * 60 * 1000;
+
 	firebase.database().ref('posts').once('value', snap => {
 		snap.forEach(post => {
-			response.write(post.key + '\n');
+			response.write(
+				post.key + ', ' + post.val().datetime + ', ' + post.val().creationTime
+			);
+			if (post.val().datetime) {
+				if (Date.now() - post.val().datetime > ONE_DAY * 3) {
+					response.write('  This should be deleted (already happened)');
+					deletePost(post.key);
+				}
+			} else if (post.val().creationTime) {
+				if (Date.now() - post.val().creationTime > ONE_DAY * 30) {
+					response.write('  This should be deleted (too old)');
+					deletePost(post.key);
+				}
+			} else {
+				response.write(
+					'  This post has no datetime or creationTime. Should it be deleted?'
+				);
+			}
+
+			response.write('\n');
 		});
 		response.end();
 	});
 });
+
+function deletePost(id) {
+	// TODO delete the post, all of its applications, and all references to the
+	// applications in their applicants' user objects
+}
 
 exports.refreshIndices = functions.https.onRequest((request, response) => {
 	// Clear old indices
@@ -69,4 +99,12 @@ exports.refreshIndices = functions.https.onRequest((request, response) => {
 	setTimeout(function() {
 		response.end();
 	}, 10000);
+});
+
+exports.getSubscribers = functions.https.onRequest((request, response) => {
+	response.write(
+		'You want the subscribers for the application: ' + JSON.stringify(request)
+	);
+
+	response.end();
 });
